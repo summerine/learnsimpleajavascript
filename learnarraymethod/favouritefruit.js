caac13d6568e1a4bee6b4a7ccbae8cc7f5d48eef
summerine <indri.pr.26@gmail.com>
2025-07-09 22:28:08 +0700

    add code comments

--- learnarraymethod/favouritefruit.js
+++ learnarraymethod/favouritefruit.js
@@ -4,15 +4,15 @@ const prompt = require("prompt-sync")();
 
 
 
-let fruits = [];
+let fruits = []; //an array
 
 for (let i = 0; i < 3; i++){
-        let fruitQuestion = prompt("What's your favourite fruit? ");
+        let fruitQuestion = prompt("What's your favourite fruit? "); //loop the question 3 times
         fruits.push(fruitQuestion); //push the value in fruitQuestion to fruits array
         
 }
 
 console.log("Your favourite fruits are: ");
 for (let i = 0; i < fruits.length; i++){
-    console.log(`${i + 1}. ${fruits[i]}`)
+    console.log(`${i + 1}. ${fruits[i]}`) //do loop equal to the length of string in array, so 3 string in array
 }
