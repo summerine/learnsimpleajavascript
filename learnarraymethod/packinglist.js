b3851e76647f1eb3077927f13a4f06376220bd43
summerine <indri.pr.26@gmail.com>
2025-07-19 00:00:58 +0700

    add comments

--- learnarraymethod/packinglist.js
+++ learnarraymethod/packinglist.js
@@ -2,22 +2,21 @@ console.log('Packing List');
 
 let prompt = require('prompt-sync')();
 
-let packingList = [];
+let packingList = []; //an empty array
 
 for(let i = 0; i < 5; i++){
     let inputItem = prompt("What item to pack? ");
-    packingList.push(inputItem);
+    packingList.push(inputItem); //push the items to the array
 }
 
 console.log('My packing list: ', packingList.join(", "));
 
 
-for(i = 0; i < packingList.length; i++){
-    let lowerItem = packingList[i].toLowerCase();
-
-    if(lowerItem === "nothing" || lowerItem === "n/a"){
-        packingList.splice(i, 1);
-        i--;
+for(i = 0; i < packingList.length; i++){ //loop over each item in the array
+    let lowerItem = packingList[i].toLowerCase(); 
+    if(lowerItem === "nothing" || lowerItem === "n/a" || lowerItem === "na"){ //check if the item is nothing, n/a or na
+        packingList.splice(i, 1); //remove the item at index i by 1
+        i--; // step back to do another looping as the array has changed 
     }
 }
 
