c4d0e5b1b7a0d6c6733fd79fe1cc0a7fb271fdbf
summerine <indri.pr.26@gmail.com>
2025-07-30 22:36:56 +0700

    dowhile

---
+++ dowhile2to10.js
@@ -0,0 +1,10 @@
+console.log("Print Even Numbers from 2 to 10");
+
+let evenNumber = 0; 
+
+do{
+    
+    console.log("Even Number: ", evenNumber);
+    evenNumber += 2;
+
+}while(evenNumber <= 10);
